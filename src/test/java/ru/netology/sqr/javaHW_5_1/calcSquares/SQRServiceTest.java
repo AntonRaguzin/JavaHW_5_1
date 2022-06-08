20ebc789861342632e815dd8faf7e3c36a3af667
antonraguzin <raguzin82@gmail.com>
2022-06-08 15:46:16 +0300

    SqrServiceTest проводит параметризированные тесты

--- src/test/java/ru/netology/sqr/javaHW_5_1/calcSquares/SQRServiceTest.java
+++ src/test/java/ru/netology/sqr/javaHW_5_1/calcSquares/SQRServiceTest.java
@@ -1,16 +1,21 @@
 package ru.netology.sqr.javaHW_5_1.calcSquares;
 
 import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
 
 public class SQRServiceTest {
-
-    //@CsvFileSource(files="src/test/resources/variables.csv")
-    @Test
-    public void calcSqrQuantity() {           //int expected, int actual) {
+    @ParameterizedTest
+    @CsvFileSource(files = "src/test/resources/variables.csv")
+    //CsvSource({
+//            "0, 10, 99",
+//            "1, 9, 99",
+//            "1, 10, 100"
+//    })
+    public void calcSqrQuantity(int expected, int min, int max) {
         SQRService service = new SQRService();
-        int expected = 2;
-        int actual = service.calcSQRService(9, 100);
+//        int expected = 0;
+        int actual = service.calcSQRService(min, max);
 
         Assertions.assertEquals(expected, actual);
     }
